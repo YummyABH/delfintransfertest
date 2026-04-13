a2d992b3f777ed1f6b91b90247af8b6edc1be816
YummyABH <harchilavaarsen2@gmail.com>
2026-04-13 20:07:24 +0300

    feat(preview): поменял блок preview по требованиям заказчика

--- src/components/BasePreview.vue
+++ src/components/BasePreview.vue
@@ -1,11 +1,54 @@
 <script setup>
+import { ref } from 'vue';
 import IconDaw from './icons/IconDaw.vue';
+import IconTelegramm from './icons/IconTelegramm.vue';
+import IconWhatsapp from './icons/IconWhatsapp.vue';
 
 const isOpen = defineModel('isOpen')
+const isOpenLink = ref(false)
+
 </script>
 
 <template>
-    <div class="w-full h-200 max-lg:h-190 overflow-y-hidden relative overflow-hidden" id="transfer">
+    <div v-show="isOpenLink" @click="isOpenLink = false"
+        class="fixed w-screen h-screen bg-black/70 flex justify-center items-center top-1/2 left-1/2 z-100 transform -translate-1/2">
+        <div class="bg-white relative border-[#e5c667] w-max  border px-10 pb-6 pt-10 ">
+            <button @click="closeModal"
+                class="cursor-pointer absolute top-2 right-2 w-8 h-8 flex items-center justify-center text-gray-400 hover:text-gray-600 transition-colors">
+                ✕
+            </button>
+            <div class="flex gap-x-2">
+                <a href="tg:@gostevoi_dom_delfine989" target="_blank">
+                    <div class="bg-[#0084c6] duration-200 cursor-pointer rounded-lg flex p-2 
+                                    transition-all 
+                                    hover:bg-[#006a9e]
+                                    hover:shadow-[inset_0_4px_10px_rgba(0,0,0,0.35)]
+                                    active:scale-95 
+                                    active:shadow-[inset_0_8px_16px_rgba(0,0,0,0.5)]">
+                        <IconTelegramm />
+                    </div>
+                </a>
+                <a href="https://wa.me/message/5VZS6ZUKG5SSE1" target="_blank">
+                    <div class="bg-[#01e675] duration-200 cursor-pointer rounded-lg flex p-2 
+                                    transition-all 
+                                    hover:bg-[#70d850]
+                                    hover:shadow-[inset_0_4px_10px_rgba(0,0,0,0.35)]
+                                    active:scale-95 
+                                    active:shadow-[inset_0_8px_16px_rgba(0,0,0,0.5)]">
+                        <IconWhatsapp />
+                    </div>
+
+                </a>
+                <a href="https://max.ru/u/f9LHodD0cOJWv4FpXUT5fnw0AlV0XkGw8P7eq1lEXDadg6m1PJNAu9eTCxU" target="_blank">
+                    <img class="w-10 h-10" src="/max.png" alt="">
+                </a>
+            </div>
+            <div class="mt-4 hover:text-zinc-500 duration-200">
+                <a href="tel:+79409626290">+7 (940) 962-62-90</a>
+            </div>
+        </div>
+    </div>
+    <div class="w-full lg:h-auto h-200 max-lg:h-200 overflow-y-hidden relative overflow-hidden" id="transfer">
         <div class="w-full overflow-hidden">
             <!-- <video class="absolute overflow-hidden inset-0 w-full h-full object-cover z-[-1]" autoplay loop muted
                 playsinline>
@@ -53,50 +96,84 @@ const isOpen = defineModel('isOpen')
                 <IconDaw />
                 <h2 class=" ">Встречаем в аэропорту Сухум</h2>
             </div>
-            <div class="w-max overflow-x-hidden flex justify-self-center mt-6">
+            <div class="w-max gap-x-4 gap-y-2 overflow-x-hidden flex justify-self-center mt-6">
                 <div
-                    class=" box-border border-3 px-10 py-3  border-white  hover:border-[#ffde7ba4]  hover:bg-[#ffde7ba4] duration-200 cursor-pointer">
+                    class=" box-border border-3 px-5 py-3  border-white  hover:border-[#ffde7ba4]  hover:bg-[#ffde7ba4] duration-200 cursor-pointer">
                     <span @click="isOpen = !isOpen">
                         Оставить заявку
+                    </span>
+                </div>
+                <div @click="isOpenLink = !isOpenLink"
+                    class="text-center w-max relative box-border border-3 px-5 py-3  border-white  hover:border-[#ffde7ba4]  hover:bg-[#ffde7ba4] duration-200 cursor-pointer">
+                    <span>
+                        Позвонить
                     </span>
                 </div>
             </div>
         </div>
 
-        <div
-            class="overflow-hidden flex left-0 max-lg:hidden h-[calc(100%-88px)] mt-22 overflow-y-hidden bottom-0  w-full relative">
-
-            <!-- <div class="border-x-3 border-[#cdad4b]">
-                <h1 class="px-10 text-center text-3xl mt-10 text-white mb-22">ЛИЦЕНЗИРОВАННЫЙ ТРАНСФЕР<br>
-                    РЕСПУБЛИКИ АБХАЗИЯ.
-                </h1>
-                <div class=" flex gap-x-6 border-y px-10 text-xl py-4">
-                    <IconDaw class="mb-0.75" />
-                    <h2 class="">VIP поездки по Абхазии</h2>
-                </div>
-                <div class=" flex gap-x-6 border-y px-10 text-xl py-4">
-                    <IconDaw class="mb-0.75" />
-                    <h2 class="">Встречаем в аэропорту Сочи, Ж/Д вокзале Адлера</h2>
-                </div>
-                <div class="flex gap-x-6  px-10 border-y text-xl py-4">
-                    <IconDaw />
-                    <h2 class="">Встречаем на границе</h2>
-                </div>
-                <div class="flex gap-x-6  px-10 border-y text-xl py-4 mb-10">
-                    <IconDaw />
-                    <h2 class=" ">Встречаем в аэропорту Сухум</h2>
-                </div>
-                <div class=" w-max overflow-x-hidden">
-                    <div @click="isOpen = !isOpen"
-                        class="relative box-border border-3 px-10 py-3 ml-10 border-white  hover:border-[#ffde7ba4]  hover:bg-[#ffde7ba4] duration-200 cursor-pointer">
-                        <span>
-                            Оставить заявку
-                        </span>
+        <div class=" flex max-lg:hidden mt-22  w-full relative ">
+            <div class="max-w-7xl w-full mx-auto px-10 max-md:px-5">
+                <div
+                    class="border-x-3 backdrop-blur-md justify-self-start bg-white/1 text-white pb-20 pt-10  border-[#cdad4b]">
+                    <div class="mb-2 flex justify-self-center w-max">
+                        <h2 class="px-4 py-3  bg-[#e5c667] mb-2 text-center text-white text-xl max-[350px]:text-lg ">Из
+                            Сочи
+                            в Абхазию -
+                            с
+                            комфортом</h2>
+                    </div>
+                    <div
+                        class="mb-6 text-center text-[15px] px-4 py-3 w-max justify-self-center  text-white bg-[#e5c667]">
+                        <div class="w-max max-w-full">
+                            Встреча - без
+                            опозданий.
+                            Граница -
+                            без
+                            хаоса.
+                        </div>
+                        <div class="w-max justify-self-center">
+                            Цена - без
+                            сюрпризов
+                        </div>
+                    </div>
+                    <h1 class="px-0 text-center text-3xl mt-0 text-white mb-6">ЛИЦЕНЗИРОВАННЫЙ ТРАНСФЕР<br>
+                        РЕСПУБЛИКИ АБХАЗИЯ.
+                    </h1>
+                    <div class=" flex gap-x-6 border-y px-10 text-xl py-4">
+                        <IconDaw class="mb-0.75" />
+                        <h2 class="">VIP поездки по Абхазии</h2>
+                    </div>
+                    <div class=" flex gap-x-6 border-y px-10 text-xl py-4">
+                        <IconDaw class="mb-0.75" />
+                        <h2 class="">Встречаем в аэропорту Сочи, Ж/Д вокзале Адлера</h2>
+                    </div>
+                    <div class="flex gap-x-6  px-10 border-y text-xl py-4">
+                        <IconDaw />
+                        <h2 class="">Встречаем на границе</h2>
+                    </div>
+                    <div class="flex gap-x-6  px-10 border-y text-xl py-4 mb-10">
+                        <IconDaw />
+                        <h2 class=" ">Встречаем в аэропорту Сухум</h2>
+                    </div>
+                    <div class=" w-max flex gap-x-4 justify-self-center overflow-x-hidden">
+                        <div @click="isOpen = !isOpen"
+                            class="relative box-border border-3 px-10 py-3  border-white  hover:border-[#ffde7ba4]  hover:bg-[#ffde7ba4] duration-200 cursor-pointer">
+                            <span>
+                                Оставить заявку
+                            </span>
+                        </div>
+                        <div @click="isOpenLink = !isOpenLink"
+                            class="relative box-border border-3 px-10 py-3  border-white  hover:border-[#ffde7ba4]  hover:bg-[#ffde7ba4] duration-200 cursor-pointer">
+                            <span>
+                                Позвонить
+                            </span>
+                        </div>
                     </div>
                 </div>
-            </div> -->
+            </div>
 
-            <div
+            <!-- <div
                 class="h-2/1 max-w-170  text-white absolute border-x-3 border-[#cdad4b] -top-14 left-70 max-lg:left-1/2 max-lg:-translate-x-1/2 z-1 -rotate-8 backdrop-blur-md bg-white/1 py-20 ">
                 <h1 class="rotate-8 px-10 text-center text-3xl mt-10 text-white mb-22">ЛИЦЕНЗИРОВАННЫЙ ТРАНСФЕР<br>
                     РЕСПУБЛИКИ АБХАЗИЯ.
@@ -126,7 +203,7 @@ const isOpen = defineModel('isOpen')
                     </div>
                 </div>
 
-            </div>
+            </div> -->
         </div>
     </div>
 </template>
